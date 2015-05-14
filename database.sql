1d8955adb2a6ad4bebc4c8308e487b19e29234e0
nico <nicolas.alphonse.bertoncello@gmail.com>
2015-05-14 16:58:02 +0200

    insert dates database

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@ register_date datetime NOT NULL,
 shop_name varchar(60),
 id_shop int(10) ,
 PRIMARY KEY (id_user)
-}ENGINE=InnoDB ;
+)ENGINE=InnoDB ;
 
 
 CREATE TABLE IF NOT EXISTS lang(
@@ -73,10 +73,9 @@ CONSTRAINT `fk_id_supplier_material_supplier` FOREIGN KEY (id_supplier) REFERENC
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS user_supplier(
-id_user_supplier int(10) NOT NULL AUTO_INCREMENT,
 id_user int(10),
 id_supplier int(10),
-PRIMARY KEY (id_user_supplier),
+PRIMARY KEY (id_user,id_supplier),
 CONSTRAINT `fk_id_user_user_supplier` FOREIGN KEY (id_user) REFERENCES user (id_user) ON DELETE NO ACTION ON UPDATE NO ACTION,
 CONSTRAINT `fk_id_supplier_user_supplier` FOREIGN KEY (id_supplier) REFERENCES supplier (id_supplier) ON DELETE NO ACTION ON UPDATE NO ACTION
 )ENGINE=InnoDB;
@@ -165,4 +164,14 @@ CREATE TABLE IF NOT EXISTS `material_combination` (
   PRIMARY KEY (`id_combination`,`id_material`)
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `craftandbudget`.`user` (`id_user`, `name`, `password`, `email`, `register_date`, `shop_name`, `id_shop`) VALUES (NULL, 'Issam Natour', 'issam', 'issamnatour90@gmail.com', '2015-05-14 00:00:00', 'BuhoPlace', '21');
+INSERT INTO `craftandbudget`.`user` (`id_user`, `name`, `password`, `email`, `register_date`, `shop_name`, `id_shop`) VALUES (NULL, 'Nicolas Alphonse', 'nicolas', 'nicoalphose@gmail.com', '2015-05-14 00:00:00', 'Supreme', '23');
 
+INSERT INTO `craftandbudget`.`supplier` (`id_supplier`, `name`, `street`, `city`, `country`, `telephone`) VALUES (NULL, 'Telas Benítez', 'Al lado de casa', 'madrid', 'spain', '912423311');
+INSERT INTO `craftandbudget`.`supplier` (`id_supplier`, `name`, `street`, `city`, `country`, `telephone`) VALUES (NULL, 'Cojines Cojones', 'Almendra', 'Alcobedas', 'spain', '919123344');
+INSERT INTO `craftandbudget`.`supplier` (`id_supplier`, `name`, `street`, `city`, `country`, `telephone`) VALUES (NULL, 'Maderas madeira', 'Severo Ochoa 29', 'Alcobedas', 'spain', '91112233');
+
+INSERT INTO `craftandbudget`.`user_supplier` (`id_user`, `id_supplier`) VALUES ('1', '2'), ('1', '1');
+INSERT INTO `craftandbudget`.`user_supplier` (`id_user`, `id_supplier`) VALUES ('2', '2'), ('2', '3');
+
+INSERT INTO `craftandbudget`.`product` (`id_product`, `id_product_prestashop`, `id_user`) VALUES (NULL, '22', '2'),(NULL, '12', '1');
